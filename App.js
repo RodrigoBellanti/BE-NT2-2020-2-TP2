a079715cad780c55b1f7509378e85cc903d423e9
srsenior <rdrg.000@gmail.com>
2020-10-08 17:58:02 -0300

    clean up

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { Button, DatePickerAndroid, StyleSheet, Text, View } from 'react-native';
-import {vibrate} from './utils'
-import  Constants  from "expo-constants";
-import Cronometro from "./components/CronometroPomodoro"
+
+import React from 'react';
+import {StyleSheet, Text, View } from 'react-native';
 import CronometroPomodoro from './components/CronometroPomodoro';
 
 
